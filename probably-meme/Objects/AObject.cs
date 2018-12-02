6066481b8f2b855d0ef18dc77587034325ab2227
Maslianko Taras <tarasmaslianko@gmail.com>
2018-12-02 18:44:18 +0200

    Object, Player, Weapon
    first

---
+++ probably-meme/Objects/AObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+
+namespace probably_meme.Objects
+{
+    class AObject
+    {
+        protected Vector2 coordinates;
+        protected double damage;
+        protected Texture2D texture;
+
+        public abstract void move();
+        public abstract void draw();
+    }
+}
---
+++ probably-meme/Objects/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace probably_meme.Objects
+{
+    class Player : AObject
+    {
+        public override void move()
+        {
+            KeyboardState state = Keyboard.GetState();
+            if (state.IsKeyDown(Keys.A) || state.IsKeyDown(Keys.Left)
+                coordinates.X++;
+        }
+
+        public override void draw()
+        {
+            
+        }
+    }
+}

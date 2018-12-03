1a07633f898aeac3b4d1b3ddc167ec1a7fd16066
Hammak <saint3543jakuzi@gmail.com>
2018-12-03 13:23:02 +0200

    Added weapon recoil

--- probably-meme/Objects/Bullet.cs
+++ probably-meme/Objects/Bullet.cs
@@ -14,8 +14,11 @@ namespace probably_meme.Objects
         public Bullet(Vector2 _vector, double _damage, Texture2D _texture, double _collisionRadius, float _speed)
             : base(_vector, _damage, _texture, _collisionRadius)
         {
+            Random random = new Random();
             MouseState state = Mouse.GetState();
+            Vector2 recoilVector = new Vector2((float)random.Next(10) / 100, (float)random.Next(10) / 100);
             vector = GameStaff.countUnitVector(new Vector2(state.X, state.Y), coordinates);
+            vector += recoilVector;
             vector.X *= _speed;
             vector.Y *= _speed;
         }
